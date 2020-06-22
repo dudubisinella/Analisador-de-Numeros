f5f6b41c1c5aab584c9fa39332d5ea171eb9b709
dudubisinella <64981838+dudubisinella@users.noreply.github.com>
2020-06-22 17:37:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+
+    let num = document.getElementById("txtn")
+    let lista = document.getElementById("lista")
+    let res = document.getElementById("res")
+    let valores = []
+
+ 
+    function isNumero(n) {
+        if (Number(n) >= 1 && Number(n) <= 100) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    function isLista(n, l) {
+        if(l.indexOf(Number(n)) != -1) {
+            return true
+        } else {
+            return false
+        }
+    }
+ 
+ 
+ 
+ 
+    function adicionar() {
+
+    if (isNumero(num.value) && !isLista(num.value, valores)) {
+
+    valores.push(Number(num.value))
+    let item = document.createElement('option')
+    item.text = `Valor ${num.value} adicionado`
+
+    lista.appendChild(item)
+    res.innerHTML = ""
+
+    }else {
+        alert("Valor inválido ou ja adicionado a lista")
+    }
+
+    num.value = ""
+    num.focus()
+  }
+
+  function finalizar() {
+      if(valores.length == 0) {
+          alert("Campo Vazio")
+      } else {
+          let total = valores.length 
+          let maior = valores[0]
+          let menor = valores[0]
+          let soma = 0
+          let media = 0
+
+
+          for (let posicao in valores) {
+              soma += valores[posicao]
+              if (valores[posicao] > maior){
+                  maior = valores[posicao]
+              }
+              if (valores[posicao] < menor) {
+                  menor = valores[posicao]
+              }
+          }
+
+          media += soma / total
+          res.innerHTML = ""
+          res.innerHTML += `<p>Ao todo temos ${total} valores cadastrados</p>`
+          res.innerHTML += `<p>O maior valor adicionado foi ${maior}</p>`
+          res.innerHTML += `<p>O menor valor adicionado foi ${menor}</p>`
+          res.innerHTML += `<p>A soma dos valores adicionados é de ${soma}</p>`
+          res.innerHTML += `<p>A média dos valores adicionados é de ${media}</p>`
+      }
+  }
